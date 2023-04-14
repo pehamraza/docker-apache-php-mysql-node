09b507ae263080f03cd12a5a5ef0f6aad492e6e3
Peham Raza <pehamraza@gmail.com>
2023-04-14 12:48:33 +0500

    add dockerfile with configs

---
+++ build-dockerfile.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PHP_VERSIONS="5.6 7.0 7.1 7.2 7.3 7.4 8.0 8.1"
+
+for PHP_VERSION in $PHP_VERSIONS;
+do
+    PHP_PACKAGES=$(cat "packages-php$PHP_VERSION.txt" | xargs)
+
+    sed -i "s/\#\#php-packages-${PHP_VERSION}\#\#/$PHP_PACKAGES/" Dockerfile
+done
---
+++ entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# use default or specified PHP version
+phpVersion="7.4"
+
+if [ -n "${PHP_VERSION}" ] && [[ "${PHP_VERSION}" =~ ^5.6|7.[0-4]|8.[0-1]$ ]]
+then
+    phpVersion="${PHP_VERSION}"
+fi
+
+echo "Using PHP version ${phpVersion}"
+
+sudo update-alternatives --set php "/usr/bin/php${phpVersion}" &>/dev/null
+sudo a2dismod 'php*' &>/dev/null
+sudo a2enmod "php${phpVersion}" &>/dev/null
+
+set -e
+
+args="$@"
+
+if [[ "$-" =~ i ]]
+then
+    # interactive
+    /bin/bash --login -i -c "${args}"
+else
+    # non-interactive
+    /bin/bash --login -c "${args}"
+fi
